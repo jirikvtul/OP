2fdf9d03df07967b0461d4b72921b106e9b882e1
Jiří Kvajsar <jiri.kvajsar@tul.cz>
2025-09-18 12:11:50 +0200

    #1 První hodina

---
+++ 1. hodina/HelloWorld/src/kin/op/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kin.op;
+
+/**
+ *
+ * @author Jiřik
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); /** sout + tab vyplní metodu */
+        String jmeno = "Jiriku";
+        System.out.println("Ahoj " + jmeno + "!"); /** sout + tab vyplní metodu */
+    }
+    
+}
